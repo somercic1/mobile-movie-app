3f2aa40973d0e1fc4590945fb8f7cc4e27d06b7a
Smajl <somercic1@etf.unsa.ba>
2025-09-11 10:24:20 +0200

    Fully functional routing system and custom nav bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -10,7 +10,7 @@ const TabIcon = ({focused, icon, title}: any) => {
        return (
            <ImageBackground
                source={images.highlight}
-               className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
+               className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden"
            >
                <Image source={icon}
                       tintColor="#151312" className="size-5"/>
@@ -32,7 +32,28 @@ const TabIcon = ({focused, icon, title}: any) => {
 
 const _Layout = () => {
   return (
-      <Tabs>
+      <Tabs
+        screenOptions={{
+          tabBarShowLabel: false,
+          tabBarItemStyle: {
+              width: '100%',
+              height: '100%',
+              justifyContent: 'center',
+              alignItems: 'center',
+          },
+            tabBarStyle: {
+              backgroundColor: '#0f0D23',
+                borderRadius: 50,
+                marginHorizontal: 20,
+                marginBottom: 36,
+                height: 52,
+                position: 'absolute',
+                overflow: 'hidden',
+                borderWidth: 1,
+                borderColor: '#0f0D23',
+            }
+        }}
+      >
           <Tabs.Screen
             name="index"
             options={{
