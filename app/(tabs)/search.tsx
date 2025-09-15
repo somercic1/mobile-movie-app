59257b5a1c31ac9d68ad21fb5fae528ae04b3a5f
Smajl <somercic1@etf.unsa.ba>
2025-09-16 01:09:06 +0200

    Show trending movies

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -32,9 +32,6 @@ const Search = () => {
         const timeoutId = setTimeout(async () => {
             if (searchQuery.trim()) {
                 await loadMovies();
-
-                if(movies?.length > 0 && movies?.[0])
-                    await updateSearchCount(searchQuery, movies[0]);
             } else {
                 reset()
             }
@@ -42,6 +39,12 @@ const Search = () => {
 
         return () => clearTimeout(timeoutId);
     }, [searchQuery]);
+
+    useEffect(() => {
+        if(movies?.length > 0 && movies?.[0]) {
+            updateSearchCount(searchQuery, movies[0]);
+        }
+    }, [movies]);
 
     return (
         <View className="flex-1 bg-primary">
